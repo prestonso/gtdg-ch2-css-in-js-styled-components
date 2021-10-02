246b08083a1a175d6b23fb9a7d49e14b10896e57
Preston So <pso@post.harvard.edu>
2021-10-02 12:03:33 -0300

    Add header styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
+// src/pages/index.js
 import React from "react"
+import styled from "styled-components"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Header = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.5rem;
+  background-color: blue;
+  color: white;
+`
+
+export default function Home({ children }) {
+  return (
+    <Header>
+      <div>Welcome to my Gatsby site!</div>
+    </Header>
+  )
 }
